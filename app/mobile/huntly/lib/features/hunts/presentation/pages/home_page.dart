571c290cf86890e84d7fb2b52d5a4a67aab5c7bf
Mehul Todi <75692643+kid-116@users.noreply.github.com>
2022-10-14 00:31:34 +0530

    Implement hunt card

---
+++ app/mobile/huntly/lib/features/hunts/presentation/pages/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:huntly/features/hunts/presentation/widgets/hunt_card.dart';
+
+import '../../../../core/theme/theme.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkTheme.colorScheme.background,
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Hello!",
+              style: darkTheme.textTheme.overline,
+            ),
+            Text(
+              'John Doe',
+              style: darkTheme.textTheme.headline2,
+            ),
+            HuntCard(title: "Manipal Institute of Technology Hunt!", location: "MIT Udupi", seatsLeft: 5, start: DateTime(2022, 9, 5, 17, 30))
+          ]
+        ),
+      ),
+    );
+  }
+}
--- app/mobile/huntly/lib/features/hunts/presentation/widgets/hunt_card.dart
+++ app/mobile/huntly/lib/features/hunts/presentation/widgets/hunt_card.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+
+import '../../../../core/theme/theme.dart';
 
 class HuntCard extends StatelessWidget {
   final String title;
@@ -18,17 +23,103 @@ class HuntCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    const borderRadius = Radius.circular(10);
+
+    return IntrinsicHeight(
       child: Row(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text()
-            ],
+          Container(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                    right: BorderSide(
+                      color: start.isBefore(DateTime.now())
+                        ? darkTheme.indicatorColor
+                        : darkTheme.highlightColor,
+                      width: 5,
+                    )
+                )
+              ),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(topLeft: borderRadius, bottomLeft: borderRadius),
+                  color: darkTheme.colorScheme.secondary,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      start.day.toString(),
+                      style: GoogleFonts.poppins(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w600,
+                      )
+                    ),
+                    Text(
+                      DateFormat('MMM').format(start),
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400
+                      )
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      DateFormat('HH:MM').format(start),
+                      style: GoogleFonts.poppins(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic,
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(topRight: borderRadius, bottomRight: borderRadius),
+                color: darkTheme.colorScheme.primary,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                Container(
+                  child: Text(
+                    title,
+                    style: darkTheme.textTheme.headline3,
+                  ),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      child: Text(
+                        location,
+                        style: TextStyle(
+                          color: darkTheme.colorScheme.secondary,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      "$seatsLeft seats left",
+                      style: TextStyle(
+                        color: darkTheme.colorScheme.secondary,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ]),
+            ),
           )
         ],
-      )
+      ),
     );
   }
 }
