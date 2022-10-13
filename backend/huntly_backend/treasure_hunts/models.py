5063cf3281a41a3e8553e7bd4559baedb3291d37
parth <mittalparth22@gmail.com>
2022-10-13 21:53:46 +0530

    Add models for TreasueHunt app

--- backend/huntly_backend/treasure_hunts/models.py
+++ backend/huntly_backend/treasure_hunts/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+from email.policy import default
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+class TreasureHunt(models.Model):
+    name =  models.CharField(max_length = 256)
+    started_at = models.DateField(auto_now_add = True)
+    ended_at = models.DateField(auto_now_add = True)
+    location_latitude = models.CharField(max_length = 30)
+    location_longitude = models.CharField(max_length = 30)
+    total_seats = models.IntegerField()
+    team_size = models.IntegerField()
+    no_of_clues = models.IntegerField()
+    theme = models.ForeignKey('Theme', blank=False, null=False, on_delete = models.CASCADE)
+
+    def _str_(self):
+        return self.name
+
+class Clue(models.Model):
+    treasure_hunt = models.ForeignKey(TreasureHunt, blank=False, null = False, on_delete=models.CASCADE)
+    step_no = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateField(auto_now_add = True)
+    answer_description = models.TextField(blank=True, null=True)
+    answer_latitude = models.CharField(max_length = 30)
+    answer_longitude = models.CharField(max_length = 30)
+    is_qr_based = models.BooleanField()
+
+    def _str_(self):
+        return self.description
+
+class Theme(models.Model):
+    name = models.CharField(max_length = 256)
+    description = models.TextField(blank=True, null=True)
+
+    def _str_(self):
+        return self.name
+
+class Team(models.Model):
+    treasure_hunt = models.ForeignKey(TreasureHunt, blank=False, null = False, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 256)
+    team_members = models.ManyToManyField(User)
+    logo = models.ImageField(upload_to='team_logos', blank=True, null=True)
+    treasure_hunt = models.ForeignKey(TreasureHunt, blank=False, null = False, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.name
+
+class TeamProgress(models.Model):
+    clue = models.ForeignKey(Clue, blank=False, null = False, on_delete=models.CASCADE)
+    solved_at = models.DateField(auto_now_add = True)
+    team = models.ForeignKey(TreasueHunt, blank= False, null = False, on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.clue.id + ' ' + self.team.name
