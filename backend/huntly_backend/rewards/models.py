b99fb7d894c27eae233bbf8230dfc4fec201ea15
parth <mittalparth22@gmail.com>
2022-10-13 21:54:04 +0530

    Add models for Rewards app

--- backend/huntly_backend/rewards/models.py
+++ backend/huntly_backend/rewards/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length = 256)
+    logo = models.ImageField(upload_to='brand_logos', blank=True, null=True)
+    added_at = models.DateField(auto_now_add = True)
+
+    def _str_(self):
+        return self.name
+
+class CouponDescription(models.Model):
+    brand = models.ForeignKey(Brand, blank=False, null = False, on_delete=models.CASCADE)
+    terms_and_conditions = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length = 256)
+    description = models.TextField(blank=True, null=True)
+
+    def _str_(self):
+        return self.title
+
+class Coupon(models.Model):
+    coupon = models.ForeignKey(CouponDescription, blank=False, null = False, on_delete=models.CASCADE)
+    code = models.CharField(max_length = 256)
+    treasure_hunt = models.ForeignKey(TreasureHunt, blank=True, null = True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, blank=True, null = True, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add = True)
+    expiry_date = models.DateField()
+
+    def _str_(self):
+        return self.code
